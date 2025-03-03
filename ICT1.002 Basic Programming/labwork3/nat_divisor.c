3c9e13d759798b29b0ea86db2dbb47fe6efdc45b
Duy-hoàng Lê <leduyhoang.ldh@gmail.com>
2025-03-03 09:28:02 +0700

    Ongoing Labwork3

--- ICT1.002 Basic Programming/labwork3/nat_divisor.c
+++ ICT1.002 Basic Programming/labwork3/nat_divisor.c
@@ -3,13 +3,21 @@
 int main()
 {
     // prompt for input
-    int nat_num;
-    do
+    int nat_num = -1; printf("Enter a natural number (n > 1): "); scanf("%d", &nat_num);
+    while (nat_num <= 1)
     {
-        printf("Enter a natural number (n > 1): ");
+        printf("Please enter a natural number (n > 1): ");
         scanf("%d", &nat_num);
-    }
-    while (nat_num <= 0);
+    };
 
     // find all divisors of input
+    int divisor_arr[sizeof(nat_num)] = {}; int i;
+    for (i = 1; i <= nat_num; i++)
+    {
+        if (nat_num % i == 0) divisor_arr[i - 1] = i;
+    }
+    for (i = 0; i <= sizeof(divisor_arr); i++)
+    {
+        printf("%d", divisor_arr[i]);
+    }
 }
